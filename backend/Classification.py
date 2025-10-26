4d78d91063fd59d2cc620f3bc41c1df632d1c213
Nathan Qiu <nathanqiu@ucsb.edu>
2025-10-25 17:28:02 -0700

    classifcation changes

--- backend/Classification.py
+++ backend/Classification.py
@@ -1,12 +1,12 @@
 import asyncio
 import os
-import ast
+import json
 from openai import AsyncAzureOpenAI
 from dotenv import load_dotenv
 load_dotenv()
 ENDPOINT = "https://unwrap-hackathon-oct-20-resource.cognitiveservices.azure.com/"
 API_KEY = os.getenv("subscription_key")
-MODEL = "gpt-5-nano"
+MODEL = "gpt-5-mini"
 REASONING = "low"
 client = AsyncAzureOpenAI(
     api_key=API_KEY,
@@ -14,6 +14,7 @@ client = AsyncAzureOpenAI(
     api_version="2024-12-01-preview"
 )
 async def analyze_comment(reviews: list[str]) -> dict[str, list[int]]:
+    print(reviews)
     prompt = f"""
     Given is a list of Reddit comments reviewing a product, 
     analyze each review and rate it from 0-5 for:
@@ -25,7 +26,7 @@ async def analyze_comment(reviews: list[str]) -> dict[str, list[int]]:
 
     If the review doesn't relate to a metric, rate it -1. If it's not related to any of the metrics rate its credibility -1. 
 
-    Return ONLY a Python dictionary in this format where the reviews are the keys and the values are lists of integers of the metrics. Do not return any questions because of the size of the dataset. 
+    Return ONLY a JSON in this format where the reviews are the keys and the values are lists of integers of the metrics. Do not reason or question. 
     {{
         "original review comment": [quality, cost, availability, utility, credibility],
         "second review": [quality, cost, availability, utility, credibility],
@@ -35,13 +36,14 @@ async def analyze_comment(reviews: list[str]) -> dict[str, list[int]]:
     Reviews: {reviews}
     """
     
-    response = await client.chat.completions.create(model=MODEL, messages=[{"role": "user", "content": prompt}], max_completion_tokens=5000, reasoning_effort= REASONING)
+    response = await client.chat.completions.create(model=MODEL, messages=[{"role": "user", "content": prompt}], max_completion_tokens=10000, reasoning_effort= REASONING)
     r = response.choices[0].message.content
+    print(r)
 
     try:
-        r = ast.literal_eval(r)
-    except Exception as e:
-        print("Failed to parse output:", e)
+        r = json.loads(r)
+    except json.JSONDecodeError as e:
+        print("Failed to parse JSON:", e)
         print(r)
         r = {}
     
